ed03bc608a31822d3d74ca5c75cb7e4a126d11a7
AlexisHucteau <alexis.hucteau@inserm.fr>
2021-11-19 14:23:32 +0100

    update

---
+++ Analysis/Wetlab/Making_plot_facs.R
@@ -0,0 +1,6 @@
+setwd("GitHub/PhD_project/Analysis/Wetlab/")
+
+Diff_facs_analysis <- read.csv("Data/Diff_AGI.csv")
+Rhod2_facs_analysis <- read.csv("Data/Rhod2_AGI.csv")
+PpIX_facs_analysis <- read.csv("Data/PpIX_AGI_ALA_Basal.csv")
+Scenith_analysis <- read.csv("Data/Scenith.csv")
